ab3766d92cabb1db88b3280d5a8ce0edc8d69d90
Utkarsh <catci142@gmail.com>
2025-01-20 15:07:40 +0530

    test

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Canvas from "./panels/Canvas";
 import "./App.css";
 // change by himanbshu
 //change by utkarsh
-//in br1
+//in br1 br1 br1 br1
 const App = () => {
   const [zoomLevel, setZoomLevel] = useState(1); // Manage zoom level at the parent level
 
