db493256c8177dad7beecc42c8738b5cf832acef
Himanshu <himanshuu932@gmail.com>
2025-01-20 15:16:39 +0530

    jio

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Canvas from "./panels/Canvas";
 import "./App.css";
 // change by himanbshu
 //change by utkarsh
+
 const App = () => {
   const [zoomLevel, setZoomLevel] = useState(1); // Manage zoom level at the parent level
 
