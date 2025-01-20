6335fd49c21d5aace741040fd256e5d07fbc0649
Himanshu <himanshuu932@gmail.com>
2025-01-20 15:18:30 +0530

    ji

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const App = () => {
       //  e.preventDefault();
       }
     };
-
+//utarsh wala 
     // Allow vertical and horizontal scrolling, but prevent zoom actions
     document.addEventListener('wheel', preventZoom, { passive: false }); // Prevent mouse wheel zoom
     document.addEventListener('touchmove', preventZoom, { passive: false }); // Prevent pinch zoom
